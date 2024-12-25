6d3de23bcd8bffceb3b8a2b791f285170cf92e12
cynerj <cyrin.neeraj@gmail.com>
2024-12-25 15:11:00 +0530

    Removing folder. Check OK.

--- TooLQit/CaLQ/Version_1.0.0/utilities/initiate/non_interactive/validate.py
+++
@@ -1,8 +0,0 @@
-from utilities.data_classes import NonInteractiveInputParameters
-from utilities.validate import checkIfFilesExist
-
-def validateNonInteractiveInput(non_interactive_input_parameters: NonInteractiveInputParameters):
-    """
-    to valide non-interactive mode input
-    """
-    checkIfFilesExist([non_interactive_input_parameters.input_card_path, non_interactive_input_parameters.input_values_path])
--- TooLQit/CaLQ/Version_1.0.0/utilities/welcome.py
+++
@@ -1,8 +0,0 @@
-def printBanner():
-    try:
-        with open("banner", encoding="utf8") as f:
-            contents = f.read()
-            print(f"{contents}")
-    except OSError:
-        print("CaLQ: Calculator for LHC limits on leptoquarks")
-        print("Version 1.0.0")
